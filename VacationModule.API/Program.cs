9e51e389b1836e714c6b58dc5bd4c6a39359445d
Tăbăcaru Andrei <andrei.tabacaru2001@yahoo.com>
2023-03-10 16:11:22 +0200

    Update NationalHoliday's files and add its controller
    This commit updates files related to national holidays and it adds a controller for national holidays. The updates include:
    
    - small fixes
    
    - service method GetListToDictionary which converts the List of national holidays into a dictionary with HolidayDate as key and HolidayName as value; this method will be to check if a date is a national holiday in O(1) time
    
    - unit tests for GetListToDictionary
    
    - use DateOnlyTimeOnly.AspNet.Swashbuckle NuGet package to be able to use string format in json requests for DateOnly because it is not supported yet by Microsoft and the given format had wierd behavior
    
    - delete duplicate data members validation and unit tests because they are unnecesary

--- VacationModule.API/Program.cs
+++ VacationModule.API/Program.cs
@@ -1,13 +1,29 @@
+using DateOnlyTimeOnly.AspNet.Converters;
+using VacationModule.Core.ServiceContracts;
+using VacationModule.Core.Services;
+using Microsoft.EntityFrameworkCore;
+using VacationModule.Infrastructure.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
 builder.Services.AddControllers();
+builder.Services.AddSingleton<INationalHolidaysService, NationalHolidaysService>();
+/*builder.Services.AddDbContext<ApplicationDbContext>
+    (options => {
+        options.UseSqlServer(builder.Configuration
+            .GetConnectionString("DefaultConnection"));
+    });*/
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => c.UseDateOnlyTimeOnlyStringConverters());
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
